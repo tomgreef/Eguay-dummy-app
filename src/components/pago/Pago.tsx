d969fc0b7f87a371c22db0d958176d723eaeafdf
pabenito <pedrobenitowork@gmail.com>
2022-06-01 21:22:02 +0200

    file formated

--- src/components/pago/Pago.tsx
+++ src/components/pago/Pago.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react'
-import Box from '@mui/material/Box';
+import {FC} from 'react'
 import Typography from '@mui/material/Typography';
 import NavBar from "../app/NavBar";
-import {Button, Card, Checkbox, FormControlLabel, FormGroup, Stack} from "@mui/material";
+import {Button, Checkbox, FormControlLabel, FormGroup, Stack} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import {DesktopDatePicker} from "@mui/x-date-pickers";
-import Login from "../login/Login";
-import {FC, useState} from "react";
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
-import {DateTimePicker} from "@mui/x-date-pickers/DateTimePicker";
 import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
-import Index from "../app/Index";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const Pago: FC = () => {
     const [value, setValue] = React.useState<Date | null>(
@@ -31,85 +27,42 @@ const Pago: FC = () => {
 
     return (
         <>
-            <NavBar hideLinks={true} />
-            <Box sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "84vh"
-            }}>
-                <Card sx={{
-                    width: "68%",
-                    height: "100%"
-                }}>
-                    <Grid container
-                        direction="row"
-                        justifyContent="center"
-                    >
-                        <Grid
-                            container
-                            justifyContent="center"
-                            alignContent="center"
-                            sx={{
-                                width: "30%",
-                                height: "100hv",
-                                padding: 4,
-                                backgroundImage : "url('http://localhost:3000/img/pagoBackgrond.png')"
-                            }}>
-                            <Typography variant='h1' align="center" alignContent="center" color="white">
-                                ¡Muchas gracias por su Compra!
-                            </Typography>
+            <NavBar hideLinks={true}/>
+
+            <Grid container display="flex" direction="row" justifyContent="center">
+                <Grid container display="flex" justifyContent="center" alignContent="center">
+                    <Typography variant='h1' align="center" alignContent="center" color="white"> ¡Muchas gracias por su Compra!</Typography>
+                </Grid>
+                <Grid container direction="column" justifyContent="center" alignItems="center">
+                    <Stack spacing={5}>
+                        <Grid item justifyContent="center" alignItems="center">
+                            <Typography variant='h1'>Detalles de su pago</Typography>
                         </Grid>
-                        <Grid
-                            container
-                            direction="column"
-                            justifyContent="center"
-                            alignItems="center"
-                            sx={{
-                                width: "70%",
-                                height: "100%",
-                                padding: 4
-                            }}>
-                            <Stack spacing={5}>
-                                <Grid item justifyContent="center" alignItems="center">
-                                    <Typography variant='h1'>Detalles de su pago</Typography>
-                                </Grid>
-                                <Stack spacing={2}>
-                                    <Grid container justifyContent="flex-start">
-                                        <TextField label="Titular" variant="filled" required />
-                                    </Grid>
-                                    <Grid container direction="row" justifyContent="flex-start" alignContent="center">
-                                        <Stack direction="row" spacing={3}>
-                                            <TextField label="Numero de tarjeta" variant="filled" required />
-                                            <TextField
-                                                label="CVC" variant="filled" type="password"
-                                                sx={{width: "80px"}}
-                                                required/>
-                                        </Stack>
-                                    </Grid>
-                                    <Grid container direction="column" justifyContent="flex-start" alignContent="flex-start">
-                                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                            <DesktopDatePicker
-                                                label="Fecha de caducidad"
-                                                inputFormat="MM/yyyy"
-                                                value={value}
-                                                onChange={handleChange}
-                                                renderInput={(params) => <TextField {...params} />}
-                                            />
-                                        </LocalizationProvider>
-                                    </Grid>
-                                    <Grid container justifyContent="flex-start">
-                                        <FormGroup>
-                                            <FormControlLabel control={<Checkbox defaultChecked />} label="Guardar mi tarjeta para futuras compras" />
-                                            <Button variant="contained" onClick={pagar}>Pagar ahora</Button>
-                                        </FormGroup>
-                                    </Grid>
+                        <Stack spacing={2}>
+                            <Grid container justifyContent="flex-start">
+                                <TextField label="Titular" variant="filled" required/>
+                            </Grid>
+                            <Grid container direction="row" justifyContent="flex-start" alignContent="center">
+                                <Stack direction="row" spacing={3}>
+                                    <TextField label="Numero de tarjeta" variant="filled" required/>
+                                    <TextField label="CVC" variant="filled" type="password" sx={{width: "80px"}} required/>
                                 </Stack>
-                            </Stack>
-                        </Grid>
-                    </Grid>
-                </Card>
-            </Box>
+                            </Grid>
+                            <Grid container direction="column" justifyContent="flex-start" alignContent="flex-start">
+                                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <DesktopDatePicker label="Fecha de caducidad" inputFormat="MM/yyyy" value={value} onChange={handleChange} renderInput={(params) => <TextField {...params} />}/>
+                                </LocalizationProvider>
+                            </Grid>
+                            <Grid container justifyContent="flex-start">
+                                <FormGroup>
+                                    <FormControlLabel control={<Checkbox defaultChecked/>} label="Guardar mi tarjeta para futuras compras"/>
+                                    <Button variant="contained" onClick={pagar}>Pagar ahora</Button>
+                                </FormGroup>
+                            </Grid>
+                        </Stack>
+                    </Stack>
+                </Grid>
+            </Grid>
         </>
     );
 }
