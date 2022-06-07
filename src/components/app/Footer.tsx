a83631dd4de4b099643769aeed88f12208931a93
Tom van Greevenbroek <tom.v.g@outlook.com>
2022-06-07 18:56:30 +0200

    FAQ

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -1,11 +1,15 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
 import { FC } from 'react'
 
 const Footer: FC = () => {
     return (
-        <Box sx={{ display: "flex", justifyContent: "center", width: "100%", padding: "40px 20px", borderTop: "1px solid #6F6C90" }}>
-            <Typography sx={{ color: "#6F6C90", fontStyle: "italic", alignSelf: "center" }}>Copyright &copy; {(new Date().getFullYear())} EGUAY | All Rights Reserved</Typography>
-        </Box>
+        <>
+            <Box sx={{ display: "flex", justifyContent: "center", width: "100%", padding: "40px 20px", borderTop: "1px solid #6F6C90" }}>
+                <Typography sx={{ color: "#6F6C90", fontStyle: "italic", alignSelf: "center", paddingRight: 10 }}>Copyright &copy; {(new Date().getFullYear())} EGUAY | All Rights Reserved</Typography>
+                <Typography sx={{ color: "#6F6C90", fontStyle: "italic", alignSelf: "center", paddingLeft: 10, textDecoration: "none" }}><a target="_blank" href='https://www.subastasprocuradores.com/faq'>FAQ</a></Typography>
+            </Box>
+
+        </>
     )
 }
 
